48f8c43ccbabc770bfe1c47667170378caaaefe0
John Maya <jcmaya@hotmail.com>
2012-09-22 17:14:17 -0400

    added sql and postgress for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'category'
+require 'task'
+YAML.load_file(Rails.root + "db/categories.yaml").each do |c|
+  attr = c.attributes
+  attr.delete("id")
+  attr.delete("created_at")
+  attr.delete("updated_at")
+  Category.create!(attr)
+end
+YAML.load_file(Rails.root + "db/tasks.yaml").each do |c|
+  attr = c.attributes
+  attr.delete("id")
+  attr.delete("created_at")
+  attr.delete("updated_at")
+  Task.create!(attr)
+end
