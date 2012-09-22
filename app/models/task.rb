bc1b8b55ec3412219c3c44b082cfc1cc148f7491
John Maya <jcmaya@hotmail.com>
2012-09-22 17:37:28 -0400

    Should be unique

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :name
   validates_presence_of :name
+  validates_uniqueness_of :name
   belongs_to :category
   attr_accessible :category_id, :name
 
