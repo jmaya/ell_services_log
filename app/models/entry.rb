645680aea99498e671b6f614d9ed863f12479f80
John Maya <jcmaya@hotmail.com>
2012-09-23 12:32:39 -0400

    need admin now

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ActiveRecord::Base
-  attr_accessible :date_of_service, :duration_of_service, :group_size, :task_ids
+  attr_accessible :date_of_service, :duration_of_service, :group_size, :task_ids, :user_id
   has_many :tasks, :through => :entry_tasks
   has_many :entry_tasks
   accepts_nested_attributes_for :tasks
