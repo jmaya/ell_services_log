3f5a37bbba3b5c8ae5d826ba474cd009dd676dc4
John Maya <jcmaya@hotmail.com>
2012-09-28 17:50:43 -0400

    added accessor

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :order_number
   validates_presence_of :name
   has_many :tasks
 end
